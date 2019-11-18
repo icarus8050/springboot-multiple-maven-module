ea0d6b0e35372c638a42ab21b6564b6457528f08
icarus8050 <icarus8050@naver.com>
2019-11-18 23:38:11 +0900

    data init

--- persistence/src/main/java/rc/persistence/DbSeeder.java
+++ persistence/src/main/java/rc/persistence/DbSeeder.java
@@ -2,12 +2,28 @@ package rc.persistence;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import rc.domain.Hotel;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Component
 public class DbSeeder implements CommandLineRunner {
 
+    private HotelRepository hotelRepository;
+
+    public DbSeeder(HotelRepository hotelRepository) {
+        this.hotelRepository = hotelRepository;
+    }
+
     @Override
     public void run(String... args) throws Exception {
+        Hotel marriot = new Hotel("Marriot", 5, true);
+        Hotel ibis = new Hotel("Ibis", 3, false);
+        Hotel goldenTulip = new Hotel("Golden Tulip", 4, true);
 
+        List<Hotel> hotels = Arrays.asList(marriot, ibis, goldenTulip);
+
+        this.hotelRepository.saveAll(hotels);
     }
 }
